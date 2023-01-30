a938eafa3c8772e6ac9126e1a4669f8bb77b5dd4
albertmg2023 <al396814@uji.es>
2023-01-30 17:21:21 +0100

    sum v2

--- sum/sum.cpp
+++ sum/sum.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int a;
+    int b;
     int sum=0;
-    cout<<"Enter a number: ";
-    cin>>n;
-    for(int i=1;i<n+1;i++){
+    cout<<"Enter a number a>=1: \n";
+    cin>>a;
+    cout<<"Enter b number b>=1: \n";
+    cin>>b;
+    
+    for(int i=a;i<b+1;i++){
 
 
         sum+=i;
@@ -13,7 +17,7 @@ int main(){
 
 
     }
-    cout<<"The sum from 1 to "<<n<<" is "<<sum;
+    cout<<"The sum from "<<a<< " to "<<b<<" is "<<sum;
 
     system("pause");
     return 0;
