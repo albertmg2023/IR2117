ea8e11b2a38e677d94c4b446f2686ac1ccad8c75
albertmg2023 <al396814@uji.es>
2023-01-30 16:52:39 +0100

    my hello

---
+++ hello1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+
+    string n;
+    cout<<"What's your name?\n";
+    cin>>n;
+    cout<<"Hello "<<n<<"!\n";
+
+
+
+system("pause");
+return 0;
+
+};
