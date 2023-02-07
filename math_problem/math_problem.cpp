121ee451768271ace8f75d770fba6c6eec63f525
albertmg2023 <al396814@uji.es>
2023-02-07 12:45:54 +0100

    math_proble v1

---
+++ math_problem/math_problem.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b,c;
+    cout<<"dame el coeficiente a";
+    cin>>a;
+    cout<<"dame el coeficiente b";
+    cin>>b;
+    cout<<"dame el coeficiente c";
+    cin>>c;
+    int solutions=0;
+    cout<<"x= "<<solutions;
+    
+    
+
+
+
+
+
+
+
+
+    system("pause");
+    return 0;
+}
