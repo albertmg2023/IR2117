ff0d23def4dde0aa71b52c5c7ea445ba3dc28268
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-02-16 13:32:01 +0100

    moverlinea recta(no funciona)

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -11,14 +11,19 @@ int main(int argc, char * argv[])
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
   
-  rclcpp::WallRate loop_rate(500ms);
-
-  while (rclcpp::ok()) {
-    message.linear.x=1.0;
-    message.angular.z=1.0;
+  rclcpp::WallRate loop_rate(10ms);
+  int i=0;
+  //distancia(m) de avance
+  //cuantas veces debe avanzar a 0.1m/s para llegar a la distancia d con loop rate de 0.01s
+  //si avanza a 0.1m/s para llegar a un  metro hacen falta 10s,
+  //si el looop rate es de 0.01s hacen falta 10/0.01 repeticiones
+  int n=(10/0.01);
+  while (rclcpp::ok() && (i<n)) {
+    message.linear.x=0.1;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
+    i+=1;
   }
   rclcpp::shutdown();
   return 0;
