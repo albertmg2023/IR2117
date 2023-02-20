8be9929607a948d4afa85ce041d919564a16cb0d
albertmg2023 <al396814@uji.es>
2023-02-20 22:28:29 +0100

    arith_mean V2

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main(int argc,char** argv){
 
-    double m=0;
+    double m=0 ,s=0;
+
+    int n=argc-1;
+
+    for(int i=1;i<=n;i++){
+
+        s+=argv[i];
+
+    }
+    m=s/n;
 
     cout<<"Arithmetic mean: "<<m<<endl;
 
