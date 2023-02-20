7b17d01df8db0286a374e9c4006a474b0a27d392
albertmg2023 <al396814@uji.es>
2023-02-20 23:41:12 +0100

    mode V1

--- media_modo_mediana/mode.cpp
+++ media_modo_mediana/mode.cpp
@@ -14,7 +14,29 @@ int main(int argc,char** argv){
         cin>>element;
 
     }   
-    
+    int aux,i,j;
+
+    //ordenamiento burbuja del vector elementos
+
+    for(int i=0;i<elements.size()-1;i++){
+
+        for(int j=i+1 ; j<elements.size();j++){
+
+            if(elements[i]>elements[j]){
+
+                aux=elements[i];
+                elements[i]=elements[j];
+                elements[j]=aux;
+            }
+
+        }
+
+    }
+    //cuento el numero de elementos del primer elemento de elements
+    i=0;
+    while(elements[i]==elements[0]){
+        m+=1;
+    }
     
 
     cout<<"MODE : "<<m<<endl;
