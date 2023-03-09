b1dea03fe70b4240e6ff465a38f43add10cb2190
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-03-09 12:47:32 +0100

    squared_param  en gazebo acabado,ejecuta correctamente

---
+++ tb3_ws/build/robot_trajectory/CMakeFiles/square_param.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/square_param.dir/square_param.cpp.o"
+  "square_param"
+  "square_param.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/square_param.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
