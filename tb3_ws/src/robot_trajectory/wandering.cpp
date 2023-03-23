7a1872021e7319f453e3dd3a5678928d628bd338
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-03-23 13:25:37 +0100

    wandering V-5

--- tb3_ws/src/robot_trajectory/wandering.cpp
+++ tb3_ws/src/robot_trajectory/wandering.cpp
@@ -5,16 +5,17 @@
 
 using namespace std::chrono_literals;
 
+bool stopdelante=false;
 void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
   std::cout <<" MIN_ranges[0..9]= ";
-  int min1=msg->ranges[0];
+  double min1=msg->ranges[0];
   for(int i=0;i<10;i++){
   if(msg->ranges[i]<min1){
   min1=msg->ranges[i];
   }
   }
   std::cout <<min1<< std::endl;
-  int min2=msg->ranges[350];
+  double min2=msg->ranges[350];
   std::cout <<"MIN_ranges[350..359]= ";
   for(int j=350;j<360;j++){
   if(msg->ranges[j]<min2){
@@ -22,24 +23,38 @@ void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
   }
   }
   std::cout <<min2<< std::endl;
+  if(min1<1 || min2<2){
   
+  stopdelante=true;
+  }
+  else{
+  stopdelante=false;
+  }
   
   
 }		
   
 int main(int argc, char * argv[])
 {
+  
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("wandering");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   auto subscriber = node->create_subscription<sensor_msgs::msg::LaserScan>("scan",10,topic_callback);
   
   geometry_msgs::msg::Twist message;
-  message.linear.x=0;
+  message.linear.x=0.5;
   rclcpp::WallRate loop_rate(10ms);
   
   while (rclcpp::ok()) {
-
+    if(stopdelante){
+    message.linear.x=0;
+    }
+    else{
+    
+    message.linear.x=0.5;
+    
+    }
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
