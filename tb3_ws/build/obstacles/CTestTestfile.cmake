88439a21b30f3a3171520f759ac3b89c335d72b7
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-04-08 23:08:30 +0200

    obstacle_detector _PandS
    se ha creado el publisher que publica en obstacle y el subscriber al tópico scan,he comprobado que funciona la conexión

---
+++ tb3_ws/build/obstacles/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/albertgm13/Documentos/GitHub/IR2117/tb3_ws/src/obstacles
+# Build directory: /home/albertgm13/Documentos/GitHub/IR2117/tb3_ws/build/obstacles
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
