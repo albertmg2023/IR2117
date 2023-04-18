cfd330422d76cdfc8f703257395949a65eff1bc4
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-04-18 12:53:50 +0200

    version final (funciona en webots y en gazebo)

--- tb3_ws/build/robot_trajectory/CMakeFiles/wandering.dir/DependInfo.cmake
+++ tb3_ws/build/robot_trajectory/CMakeFiles/wandering.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/albertgm13/Documentos/GitHub/IR2117/tb3_ws/src/robot_trajectory/wandering.cpp" "/home/albertgm13/Documentos/GitHub/IR2117/tb3_ws/build/robot_trajectory/CMakeFiles/wandering.dir/wandering.cpp.o"
+  "/home/usuario/Documentos/GitHub/IR2117/tb3_ws/src/robot_trajectory/wandering.cpp" "/home/usuario/Documentos/GitHub/IR2117/tb3_ws/build/robot_trajectory/CMakeFiles/wandering.dir/wandering.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- tb3_ws/src/obstacles/avoidance.cpp
+++ tb3_ws/src/obstacles/avoidance.cpp
@@ -11,6 +11,10 @@ using namespace std::chrono_literals;
 bool obstacle_front=false;
 bool obstacle_left=false;
 bool obstacle_right=false;
+bool girandoantes;
+bool giroderecha;
+bool giroizquierda;
+
 
 void callback_front(const example_interfaces::msg::Bool::SharedPtr msgfront){
 
@@ -32,7 +36,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("avoidance");
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
   auto subs_front=node->create_subscription<example_interfaces::msg::Bool>("/front/obstacle",10,callback_front);
   auto subs_left=node->create_subscription<example_interfaces::msg::Bool>("/left/obstacle",10,callback_left);
   auto subs_right=node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle",10,callback_right);
@@ -41,37 +45,90 @@ int main(int argc, char * argv[])
   //aqui introduzco FSM
   while (rclcpp::ok()) {
 
-    if(obstacle_front==true){
-
-      message.linear.x=0.0;
-      if(obstacle_left && !obstacle_right){
-        //girar derecha
-        message.angular.z=-0.1;
-      }
-      else if(obstacle_right && !obstacle_left){
-        //girar izquierda
-        message.angular.z=0.1;
-      }
-      else{
-        //girar derecha o izquierda de forma random
-      int numr=rand()%10;
-      if(numr<=4){
-        //girar izquierda
-        message.angular.z=0.1;
-      }
+    if(obstacle_front){
+     
+      if(!girandoantes ){
+    
+      	if(obstacle_left && !obstacle_right){
+        	//girar derecha
+        	message.linear.x=0.0;
+        	message.angular.z=-0.1;
+        	giroderecha=true;
+        	giroizquierda=false;
+        	girandoantes=true;
+        	
+      	}
+      	else if(obstacle_right && !obstacle_left){
+        	//girar izquierda
+        	message.linear.x=0.0;
+        	message.angular.z=0.1;
+        	giroizquierda=true;
+        	giroderecha=false;
+        	girandoantes=true;
+        	
+        	
+      	}
+      	else{
+        	//girar derecha o izquierda de forma random
+      	int numr=rand()%10;
+      	if(numr<=4){
+        	//girar izquierda
+        	message.linear.x=0.0;
+        	message.angular.z=0.1;
+        	giroizquierda=true;
+        	giroderecha=false;
+        	girandoantes=true;
+        	
+        	
+      	}
       
-      else{
-        //girar derecha
-        message.angular.z=-0.1;
-      }
+      	else{
+        	//girar derecha
+        	message.linear.x=0.0;
+        	message.angular.z=-0.1;
+        	giroderecha=true;
+        	giroizquierda=false;
+        	girandoantes=true;
+        	
+        	
+      	}
       
 
-      }
+      	}
+      	
+      	
+      	
+    
+    }
+    else{
+    if(giroizquierda){
+    	message.linear.x=0.0;
+        message.angular.z=0.1;
+    
+    }
+    else if(giroderecha){
+    
+    	message.linear.x=0.0;
+        message.angular.z=-0.1;
+    
+    
+    }
+    publisher->publish(message);
+    
+    
+    
+    
+    
+    }
     }
     //si no hay obstaculo hacia delante
-    else{
+    if (!obstacle_front ){
       //seguir hacia delante
+      
       message.linear.x=0.1;
+      message.angular.z=0.0;
+      girandoantes=false;
+      publisher->publish(message);
 
     }
     
