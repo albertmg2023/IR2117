c557c639c34a8a016684e056dec1920690b8f889
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-05-07 20:44:35 +0200

    olimpyc V9 Y V10

--- tb3_ws/olympic/src/rings.cpp
+++ tb3_ws/olympic/src/rings.cpp
@@ -24,61 +24,46 @@ int main(int argc, char * argv[])
  double m=(M_PI*2)/(w*slooprate);
 
  //el cliente de  setpen
- rclcpp::Client<turtlesim::srv::SetPen>::SharedPtr clientsetpen =node->create_client<turtlesim::srv::SetPen>("/turtle1/set_pen");
-
+  rclcpp::Client<turtlesim::srv::SetPen>::SharedPtr clientsetpen =node->create_client<turtlesim::srv::SetPen>("/turtle1/set_pen");
+  //levata lapiz
   auto requestsetpen = std::make_shared<turtlesim::srv::SetPen::Request>();
-  requestsetpen->r = 50;
-  requestsetpen->g = 0;
-  requestsetpen->b = 255;
   requestsetpen->width = 0;
   requestsetpen->off = 255;
 
-
-
   while (!clientsetpen->wait_for_service(1s)) {
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-      return 0;
-    }
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    
   }
 
   auto resultsetpen = clientsetpen->async_send_request(requestsetpen);
-  // Wait for the resu
+  
 
 //el cliente teleport_absolute
 
-rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr cliteleportabs =node->create_client<turtlesim::srv::TeleportAbsolute>("/turtle1/teleport_absolute");
-
+  rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr cliteleportabs =node->create_client<turtlesim::srv::TeleportAbsolute>("/turtle1/teleport_absolute");
+  //teletransporta a punto inicial
   auto reqteleportabs = std::make_shared<turtlesim::srv::TeleportAbsolute::Request>();
   reqteleportabs->x=2.5;
   reqteleportabs->y=6.0;
   reqteleportabs->theta=0;
-
-
-
-
+  
   while (!cliteleportabs->wait_for_service(1s)) {
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-      return 0;
-    }
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    
   }
 
+
   auto resultteleportabs = cliteleportabs->async_send_request(reqteleportabs);
+  //color del primer circulo y lapiz abajo
+  
+
+  requestsetpen = std::make_shared<turtlesim::srv::SetPen::Request>();
+  requestsetpen->r = 50;
+  requestsetpen->g = 0;
+  requestsetpen->b = 255;
   requestsetpen->off =0;
   requestsetpen->width = 2;
 
   resultsetpen = clientsetpen->async_send_request(requestsetpen);
-
-
-
-  
-
-
-
-
+  //dibuja 1er circulo
   while (rclcpp::ok() && (j<m)) {
     message.angular.z=w;
     message.linear.x=vl;
@@ -88,7 +73,57 @@ rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr cliteleportabs =node
     j+=1;
   }
   message.angular.z=0;
-  message.linear.x=0;	
+  message.linear.x=0;
+
+  //second circle(black)
+
+  //levanta lapiz
+
+ 
+
+  requestsetpen = std::make_shared<turtlesim::srv::SetPen::Request>();
+  requestsetpen->width = 0;
+  requestsetpen->off = 255;
+  resultsetpen = clientsetpen->async_send_request(requestsetpen);
+  //teletransporta a posicion del circulo2
+  reqteleportabs = std::make_shared<turtlesim::srv::TeleportAbsolute::Request>();
+
+  reqteleportabs->x=2.5+(radius*2)+(radius/6);
+  reqteleportabs->y=6.0;
+  reqteleportabs->theta=0;
+
+  resultteleportabs = cliteleportabs->async_send_request(reqteleportabs);
+  //lapiz abajo y color negro
+  
+
+  requestsetpen = std::make_shared<turtlesim::srv::SetPen::Request>();
+  requestsetpen->r = 0;
+  requestsetpen->g = 0;
+  requestsetpen->b = 0;
+  requestsetpen->width = 2;
+  requestsetpen->off = 0;
+  resultsetpen = clientsetpen->async_send_request(requestsetpen);
+  //dibujacirculo2
+  j=0;
+  while (rclcpp::ok() && (j<m)) {
+    message.angular.z=w;
+    message.linear.x=vl;
+    publisher->publish(message);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+    j+=1;
+  }
+  message.angular.z=0;
+  message.linear.x=0;
+
+
+
+
+
+
+  
+
+	
  rclcpp::shutdown();
  return 0;
 }
