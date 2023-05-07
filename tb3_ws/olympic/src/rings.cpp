b00fa7bea838b052f966943702ac31c7e889afa2
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-05-07 16:00:11 +0200

    olympic v5
    he solucionado error anterior en la formula de las iteraciones

--- tb3_ws/olympic/src/rings.cpp
+++ tb3_ws/olympic/src/rings.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include <cmath>
 
 using namespace std::chrono_literals;
 
@@ -9,16 +10,20 @@ int main(int argc, char * argv[])
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
  auto publisher = node->create_publisher<geometry_msgs::msg::Twist  >("/turtle1/cmd_vel", 10);
- 
+ node->declare_parameter("radius",1.0);
  geometry_msgs::msg::Twist message;
- rclcpp::WallRate loop_rate(50ms);
- double slooprate=0.05;
- double w=0.4;
+
+ rclcpp::WallRate loop_rate(500ms);
+ double radius=node->get_parameter("radius").get_parameter_value().get<double>();
+ double slooprate=0.5;
+ double w=0.5;
+ double vl=radius*w;
  int j=0;
- int  m=((3.14*2)/w)/slooprate;
+ double m=(M_PI*2)/(w*slooprate);
+
   while (rclcpp::ok() && (j<m)) {
     message.angular.z=w;
-    message.linear.x=0.2;
+    message.linear.x=vl;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
