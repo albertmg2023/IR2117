2525302c7194ddb80759cbabb4a66502a945b1c4
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-05-07 19:45:16 +0200

    olympic V8

--- tb3_ws/olympic/src/rings.cpp
+++ tb3_ws/olympic/src/rings.cpp
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "turtlesim/srv/set_pen.hpp"
+#include "turtlesim/srv/teleport_absolute.hpp"
 #include <cmath>
 
 using namespace std::chrono_literals;
@@ -26,11 +27,11 @@ int main(int argc, char * argv[])
  rclcpp::Client<turtlesim::srv::SetPen>::SharedPtr clientsetpen =node->create_client<turtlesim::srv::SetPen>("/turtle1/set_pen");
 
   auto requestsetpen = std::make_shared<turtlesim::srv::SetPen::Request>();
-  requestsetpen->r = 255;
-  requestsetpen->g = 255;
-  requestsetpen->b = 0;
-  requestsetpen->width = 2;
-  requestsetpen->off = 0;
+  requestsetpen->r = 50;
+  requestsetpen->g = 0;
+  requestsetpen->b = 255;
+  requestsetpen->width = 0;
+  requestsetpen->off = 255;
 
 
 
@@ -43,7 +44,35 @@ int main(int argc, char * argv[])
   }
 
   auto resultsetpen = clientsetpen->async_send_request(requestsetpen);
-  // Wait for the result.
+  // Wait for the resu
+
+//el cliente teleport_absolute
+
+rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr cliteleportabs =node->create_client<turtlesim::srv::TeleportAbsolute>("/turtle1/teleport_absolute");
+
+  auto reqteleportabs = std::make_shared<turtlesim::srv::TeleportAbsolute::Request>();
+  reqteleportabs->x=2.5;
+  reqteleportabs->y=6.0;
+  reqteleportabs->theta=0;
+
+
+
+
+  while (!cliteleportabs->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+  }
+
+  auto resultteleportabs = cliteleportabs->async_send_request(reqteleportabs);
+  requestsetpen->off =0;
+  requestsetpen->width = 2;
+
+  resultsetpen = clientsetpen->async_send_request(requestsetpen);
+
+
 
   
 
