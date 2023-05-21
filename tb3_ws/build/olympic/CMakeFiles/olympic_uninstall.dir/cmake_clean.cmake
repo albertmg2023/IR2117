46398c5c7efc17d2cb83815eccd8ac77542298eb
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2023-05-21 20:26:16 +0200

    action_exercise V0

--- tb3_ws/build/olympic/CMakeFiles/olympic_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/olympic_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/olympic_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
